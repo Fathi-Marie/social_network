d8cdaa6c3988a37e7abc6630b6d8c9e24d87beb7
fathi marie <fathi.marie1@gmail.com>
2026-04-20 09:56:05 +0200

    création du footer et du header

---
+++ socialnetwork/src/main/resources/static/js/header.js
@@ -0,0 +1,10 @@
+function toggleMobileMenu() {
+    const menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('open');
+    if (menu.classList.contains('open')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'auto';
+    }
+}
+lucide.createIcons();
