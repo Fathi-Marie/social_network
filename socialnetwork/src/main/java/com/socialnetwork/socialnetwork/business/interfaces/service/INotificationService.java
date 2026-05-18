2f7fcb061593b4381aed019a9de9297f7b050307
fathi marie <fathi.marie1@gmail.com>
2026-05-18 08:30:57 +0200

    gestion des notification

---
+++ socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/interfaces/service/INotificationService.java
@@ -0,0 +1,7 @@
+package com.socialnetwork.socialnetwork.business.interfaces.service;
+import org.springframework.http.ResponseEntity;
+import com.socialnetwork.socialnetwork.entity.Notification;
+
+public interface INotificationService {
+    ResponseEntity<Notification> save(Notification notification);
+}
